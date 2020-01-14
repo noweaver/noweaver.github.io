f3b5b6d094ceb22f66b1258bb8e66c2d64c56cb3
Ryan Guhnguk Ahn <ryan@RyanMac.local>
2020-01-14 20:15:26 +0900

    루트 위치 변경

---
+++ en.search.min.2ecefb74894c252ac7af762a31520c5d3715d3ad10fbfe323c8850b24981c9d6.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/posts/flexsearch.min.js');loadScript('/posts/en.search-data.min.e35283b941aeb117b683d96fb503b99e7a65b35a85e232e7b0640a16b1a851ae.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
