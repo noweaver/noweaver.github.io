2887d0ed0a87af6b3ab0abecb9a8f2c989b93b4b
noweaver <i.am.noweaver@gmail.com>
2020-03-25 13:31:32 +0900

    파일 정리

--- ko.search.min.6b3772df97d0c30fde0c1e89e9aee2a210b06717b0937a3c5f9ec93931c0daa8.js
+++
@@ -1,9 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
-const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
-input.focus();e.preventDefault();}
-function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/ko.search-data.min.dc141e80674cf184cfeaae08d315b1bd5b0385d710c77f91ba95f7aa4ce78cd0.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
